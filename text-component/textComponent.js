28d6c0468c8fd2461e6fd4a02682362e2a13fc7a
Aarón Martín <aamarber@gmail.com>
2023-11-27 17:55:50 +0100

    Using it as open shadowed dom

--- text-component/textComponent.js
+++ text-component/textComponent.js
@@ -2,12 +2,14 @@ class TextComponent extends HTMLElement{
     constructor(){
         super();
 
+        this.shadow = this.attachShadow({mode: 'open'});
+
         let span = document.createElement('span');
 
         span.className = 'wc-text';
         span.innerHTML = 'Initially defined text';
 
-        this.appendChild(span);
+        this.shadow.appendChild(span);
     }
 }
 
