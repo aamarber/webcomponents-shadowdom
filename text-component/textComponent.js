959a7ac126b2687c25a9a84c9394111fe123d621
Aarón Martín <aamarber@gmail.com>
2023-11-27 17:54:03 +0100

    Implemented text component without shadow dom

--- text-component/textComponent.js
+++ text-component/textComponent.js
@@ -1,6 +1,13 @@
 class TextComponent extends HTMLElement{
     constructor(){
         super();
+
+        let span = document.createElement('span');
+
+        span.className = 'wc-text';
+        span.innerHTML = 'Initially defined text';
+
+        this.appendChild(span);
     }
 }
 
